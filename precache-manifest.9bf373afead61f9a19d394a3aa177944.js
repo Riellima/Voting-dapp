a72cd6bdf5f0958e1eae4ddc52a9f60c73b190fb
Riellima <marielle.diandy@gmail.com>
2022-10-30 16:36:18 +0100

    Updates

---
+++ precache-manifest.9bf373afead61f9a19d394a3aa177944.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa909c5d36b8a9cceffef5adc832c523",
+    "url": "/Voting-dapp/index.html"
+  },
+  {
+    "revision": "53fe71f749585c915a9e",
+    "url": "/Voting-dapp/static/css/main.a183cd99.chunk.css"
+  },
+  {
+    "revision": "09bef1899493be585e6b",
+    "url": "/Voting-dapp/static/js/2.2cce6682.chunk.js"
+  },
+  {
+    "revision": "53fe71f749585c915a9e",
+    "url": "/Voting-dapp/static/js/main.67d93c7c.chunk.js"
+  },
+  {
+    "revision": "c18bf6c6ba6aae47a5f6",
+    "url": "/Voting-dapp/static/js/runtime-main.fd2f69ce.js"
+  }
+]);
